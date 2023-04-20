05c1d9ff402d4d2d02ca2cb90cf42765cfb3e87f
a6uxa4 <a6uxa4@icloud.com>
2023-04-21 00:40:51 +0600

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type Todo = {
+	id: number
+	title: string
+	completed: boolean
 }
 
-export default App;
+const initialTodos: Todo[] = [
+	{
+		id: 1,
+		title: 'Learn TypeScript',
+		completed: false,
+	},
+	{
+		id: 2,
+		title: 'Build a Todo App',
+		completed: false,
+	},
+]
+
+const App: React.FC = () => {
+	const [todos, setTodos] = useState<Todo[]>(initialTodos)
+	const [newTodo, setNewTodo] = useState<string>('')
+
+	const handleNewTodoChange = (
+		event: React.ChangeEvent<HTMLInputElement>,
+	) => {
+		setNewTodo(event.target.value)
+	}
+
+	const handleNewTodoAdd = () => {
+		const newId = todos.length + 1
+		const newTodoItem = {
+			id: newId,
+			title: newTodo,
+			completed: false,
+		}
+		setTodos([...todos, newTodoItem])
+		setNewTodo('')
+	}
+
+	const handleTodoToggle = (todoId: number) => {
+		const updatedTodos = todos.map((todo) => {
+			if (todo.id === todoId) {
+				return { ...todo, completed: !todo.completed }
+			}
+			return todo
+		})
+		setTodos(updatedTodos)
+	}
+
+	return (
+		<div>
+			<h1>Todo List</h1>
+			<div>
+				<input
+					type='text'
+					value={newTodo}
+					onChange={handleNewTodoChange}
+				/>
+				<button onClick={handleNewTodoAdd}>Add Todo</button>
+			</div>
+			<ul>
+				{todos.map((todo) => (
+					<li key={todo.id}>
+						<input
+							type='checkbox'
+							checked={todo.completed}
+							onChange={() => handleTodoToggle(todo.id)}
+						/>
+						<span>{todo.title}</span>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+
+export default App
